2ae479a50e48b14f99b86d25cb4ac84768f4c7b7
Lekic <daniel@lekic.net>
2013-10-23 19:09:00 +1100

    Generated Devise Model
    Completed 5 step guide

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module Trace
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    #Use if deploying on Heroku with Rails 3.2
+    #config.assets.initialize_on_precompile = false
   end
 end
