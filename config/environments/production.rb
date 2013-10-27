bd1cd5fd3f9b860bcd959b8337271e1a7d8a58e0
haydenbleasel <haydenbleasel@gmail.com>
2013-10-27 18:38:37 +1100

    Custom asset precomp and stuff

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ Trace::Application.configure do
   config.assets.digest = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.1'
+  config.assets.version = '1.2'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
@@ -59,7 +59,7 @@ Trace::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile = [ "landing", "pests", "welcome", "users", "sightings", "reports" ]
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
