b32d5dad80d5a34e7b4ca5d317e24d93386ffcf2
haydenbleasel <haydenbleasel@gmail.com>
2013-10-29 16:35:33 +1100

    Initiated Chosen

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,4 +41,9 @@ $(document).ready(function() {
 		}
 		return false;
 	});
+	$('.chosen-select').chosen({
+    	allow_single_deselect: true,
+    	no_results_text: 'No results matched',
+    	width: '200px'
+    });
 });
