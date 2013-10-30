0e526379e77ecd5accdde5818c2eba3910c9f0cc
Lekic <daniel@lekic.net>
2013-10-30 20:52:43 +1100

    Included validation in phone model

--- db/migrate/20131002153412_create_phones.rb
+++ db/migrate/20131002153412_create_phones.rb
@@ -1,9 +1,9 @@
 class CreatePhones < ActiveRecord::Migration
   def change
     create_table :phones do |t|
-      t.integer :country_code
-      t.integer :area_code
-      t.integer :phone
+      t.integer :country_code, null: false
+      t.integer :area_code, null: false
+      t.integer :phone, null: false
       t.references :phone_type, index: true
       t.references :user, index: true
       t.string :comment
