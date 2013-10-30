7453add978b43b1ad438b991ce3fb90c5a3cb59d
Lekic <daniel@lekic.net>
2013-10-30 19:58:10 +1100

    Overwrote devise registration controller to take more fields

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,10 +35,5 @@ class ApplicationController < ActionController::Base
       u.permit(:username, :name, :email, :password, :password_confirmation, :admin, :date_of_birth, :current_password)
     end
   end
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_in) { |u| u.permit(:username, :email) }
-    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password) }
-  end
+  before_filter :configure_devise_params, if: :devise_controller?
 end
