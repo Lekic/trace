765375ac85592cb7ea81d23b199a0f7560db8b25
Daniel <daniel@lekic.net>
2013-11-06 01:16:39 +1100

    Anti-boat documentation

--- app/controllers/documentation_controller.rb
+++ app/controllers/documentation_controller.rb
@@ -1,4 +1,8 @@
 class DocumentationController < ApplicationController
   def index
+  	if !current_user.try(:admin?)
+  		flash[:alert] = "Sorry ladies. Admins only."
+  		redirect_to index_path
+  	end
   end
 end
