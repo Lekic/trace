4ffad502087a7d754fbab31370ff3741c4b38e2c
Daniel <daniel@lekic.net>
2013-11-05 17:25:28 +1100

    Added sighting validation and sighting form labels

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -5,4 +5,10 @@ class Sighting < ActiveRecord::Base
   belongs_to :user, inverse_of: :sightings
   #belongs_to :reporter, class_name: "User"
   
+  validates :pest, presence: true
+  validates :quantity, inclusion: { :in => 1..100 }, allow_nil: true
+  validates :park, presence: true
+  validates :information, length: { maximum: 1000 }
+  validates :user, presence: true
+
 end
