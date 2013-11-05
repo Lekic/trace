ea092a22e9fe307c76e97d33feed16f49ecb0616
Daniel <daniel@lekic.net>
2013-11-05 20:07:11 +1100

    Fixed "todays additions" info on home page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -5,7 +5,9 @@ class WelcomeController < ApplicationController
   		redirect_to root_path
   	end
   	@pests = Pest.all
+  	@new_pests = Pest.find(:all, :conditions => ["DATE(created_at) = DATE(?)", Time.now])
 	@sightings = Sighting.all
+	@new_sightings = Sighting.find(:all, :conditions => ["DATE(created_at) = DATE(?)", Time.now])
 	@users = User.all
   end
 end
