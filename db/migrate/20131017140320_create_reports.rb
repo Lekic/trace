6868c4dad79f7a632a0fdce03d4be97a35773af1
Daniel <daniel@lekic.net>
2013-11-05 20:49:15 +1100

    Beginning report generation

--- db/migrate/20131017140320_create_reports.rb
+++ db/migrate/20131017140320_create_reports.rb
@@ -1,7 +1,8 @@
 class CreateReports < ActiveRecord::Migration
   def change
     create_table :reports do |t|
-      t.date :date_created
+      t.date :start_date
+      t.date :end_date
       t.references :user, index: true
       #t.references :sighting, index: true
 
