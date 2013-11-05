c3d8bafd6251d9476a11f136ea61fb4eba833f21
Daniel <daniel@lekic.net>
2013-11-06 04:54:09 +1100

    Completed report restrictions

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -22,44 +22,60 @@ class ReportsController < ApplicationController
 		@report.user = current_user
     	if @report.save
     		flash[:notice] = "Report successfully created."
-			redirect_to @report
+			redirect_to reports_path
 		else
 			render 'new'
 		end
 	end
 
 	def show
-		@report = Report.find(params[:id])
-		@sightings = Sighting.find(:all, :conditions => ["created_at between ? and ?",
-         @report.start_date-1, @report.end_date+1])
-		@final = []
-		@sightings.each do |sighting|
-			@report.area_ids.each do |area|
-				@final << sighting if sighting.park.area_id == area
+		if current_user.try(:admin?)
+			@sightings = Sighting.find(:all, :conditions => ["created_at between ? and ?",
+		     @report.start_date-1, @report.end_date+1])
+			@final = []
+			@sightings.each do |sighting|
+				@report.area_ids.each do |area|
+					@final << sighting if sighting.park.area_id == area
+				end
 			end
+		else
+			flash[:alert] = "Sorry, no can do. Come back when you're a system administrator!"
+			redirect_to index_path
 		end
+		
 	end
 
 	def edit
-		@report = Report.find(params[:id])
+		if current_user.try(:admin?)
+			@report = Report.find(params[:id])
+		else
+			flash[:alert] = "Sorry, no can do. Come back when you're a system administrator!"
+			redirect_to index_path
+		end
 	end
 
 	def update
     	@report = Report.find(params[:id])
 		if @report.update(params[:report].permit(:name, :start_date, :end_date, {:area_ids => []}))
-			redirect_to @report
+			flash[:notice] = "Report Successfuly Updated."
+			redirect_to reports_path
 		else
 			render 'edit'
 		end
 	end
 
 	def destroy
-		@report = Report.find(params[:id])
-		@report.destroy
-
-		redirect_to reports_path
+		if current_user.try(:admin?)
+			@report = Report.find(params[:id])
+			@report.destroy
+			flash[:notice] = "Yay - report successfully deleted!"
+		else
+			flash[:alert] = "Sorry, no can do. Come back when you're a system administrator!"
+		end
+		redirect_to index_path
 	end
 
+	# Permitted parameters when creating a report
 	private
       def report_params
         params.require(:report).permit(:name, :start_date, :end_date, {:area_ids => []})
