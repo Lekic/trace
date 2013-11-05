da0c0e8f5ab238fb509e7a6900f28bb37c05fc96
Daniel <daniel@lekic.net>
2013-11-05 23:09:20 +1100

    Installed timeliness and implemented

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,6 +4,6 @@ class Report < ActiveRecord::Base
   has_and_belongs_to_many :areas
   has_many :pests, through: :sightings
 
-  validates :start_date, presence: true
-  validates :end_date, presence: true#, timeliness: {after: start_date}
+  validates_datetime :start_date, presence: true
+  validates_datetime :end_date, presence: true, timeliness: {after: start_date}
 end
