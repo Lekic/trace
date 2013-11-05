f02b5952a5a53a5b13054d92ecb883e90516f469
Daniel <daniel@lekic.net>
2013-11-06 01:09:38 +1100

    Devise using formtastic and removed forgot password
    Pending Hayden's styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable,
-         :validatable, :authentication_keys => [:login]
+         :rememberable, :trackable, :validatable,
+         :authentication_keys => [:login]
   
   # Virtual attribute for authenticating by either username or email
   # This is in addition to a real persisted field like 'username'
