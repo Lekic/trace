5ae7c7d2a6efadb7722be9b049d3087316fca410
Hayden Bleasel <haydenbleasel@gmail.com>
2013-11-07 01:22:54 +1100

    Fixed application controller formatting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,42 +1,43 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
 
-  # Redirects user to specified page after sign in
-  # Access level: User
-  def after_sign_in_path_for(resource)
-    index_path
-  end
+	# Prevent CSRF attacks by raising an exception.
+	# For APIs, you may want to use :null_session instead.
+	protect_from_forgery with: :exception
 
-  # Defines the resource referred to by devise
-  def resource_name
-    :user
-  end
+	# Redirects user to specified page after sign in
+	# Access level: User
+	def after_sign_in_path_for(resource)
+		index_path
+	end
 
-  # Creates a new instance of the resource for devise
-  def resource
-    @resource ||= User.new
-  end
+	# Defines the resource referred to by devise
+	def resource_name
+		:user
+	end
 
-  # Maps the devise resource to devise mapping
-  def devise_mapping
-    @devise_mapping ||= Devise.mappings[:user]
-  end
+	# Creates a new instance of the resource for devise
+	def resource
+		@resource ||= User.new
+	end
 
-  helper_method :resource, :resource_name, :devise_mapping
+	# Maps the devise resource to devise mapping
+	def devise_mapping
+		@devise_mapping ||= Devise.mappings[:user]
+	end
 
-  before_filter :configure_devise_params, if: :devise_controller?
+	helper_method :resource, :resource_name, :devise_mapping
 
-  protected
+	before_filter :configure_devise_params, if: :devise_controller?
 
-  # Configures permitted parameters in devise sign up and edit registration
-  def configure_devise_params
-    devise_parameter_sanitizer.for(:sign_up) do |u|
-      u.permit(:username, :name, :email, :password, :password_confirmation, :admin, :date_of_birth)
-    end
-    devise_parameter_sanitizer.for(:edit) do |u|
-      u.permit(:username, :name, :email, :password, :password_confirmation, :admin, :date_of_birth, :current_password)
-    end
-  end
+	protected
+
+		# Configures permitted parameters in devise sign up and edit registration
+		def configure_devise_params
+			devise_parameter_sanitizer.for(:sign_up) do |u|
+				u.permit(:username, :name, :email, :password, :password_confirmation, :admin, :date_of_birth)
+			end
+			devise_parameter_sanitizer.for(:edit) do |u|
+				u.permit(:username, :name, :email, :password, :password_confirmation, :admin, :date_of_birth, :current_password)
+			end
+		end
 end
